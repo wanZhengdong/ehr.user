2f8214ec308a6a50660452b6fda0aafb3ab961c8
wanZhengdong <8363800@163.com>
2016-10-07 22:24:34 +0800

    rebuild

--- impl/com/pd/ehr/service/impl/UserService.java
+++ impl/com/pd/ehr/service/impl/UserService.java
@@ -1,4 +1,4 @@
-package com.pd.ehr.user.service.impl;
+package com.pd.ehr.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,7 +7,7 @@ import com.pd.ehr.user.dao.IUserDao;
 import com.pd.ehr.user.service.IUserService;
 
 @Service
-public class EhrUserService implements IUserService
+public class UserService implements IUserService
 {
     @Autowired
     private IUserDao userDao;
