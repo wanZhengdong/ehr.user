c22c0c18731074c66dc47bd6e750cdda81d2b63b
wanZhengdong <8363800@163.com>
2016-11-06 16:59:53 +0800

    rebuild

--- api/com/pd/ehr/user/vo/UserFilter.java
+++ api/com/pd/ehr/user/vo/UserFilter.java
@@ -1,8 +1,8 @@
 package com.pd.ehr.user.vo;
 
-import com.pd.ehr.vo.IEhrFilterVO;
+import com.pd.api.IFilterVO;
 
-public class UserFilter extends User implements IEhrFilterVO
+public class UserFilter extends User implements IFilterVO
 {
     
 }
